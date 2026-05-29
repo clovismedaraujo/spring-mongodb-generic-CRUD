d6d458093497f4b3c9f16f6de9f67b2a92ee4982
Clovis Luan <luanmdearaujo@gmail.com>
2026-05-28 23:12:40 -0300

    Initialize project with Spring Boot, MongoDB dependencies.

---
+++ src/main/java/com/clovis/trabalho_ii_unidade_nosql/TrabalhoIiUnidadeNosqlApplication.java
@@ -0,0 +1,13 @@
+package com.clovis.trabalho_ii_unidade_nosql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoIiUnidadeNosqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoIiUnidadeNosqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clovis/trabalho_ii_unidade_nosql/TrabalhoIiUnidadeNosqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clovis.trabalho_ii_unidade_nosql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoIiUnidadeNosqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
